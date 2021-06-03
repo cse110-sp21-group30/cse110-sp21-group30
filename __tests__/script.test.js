d221aba925660bdd975205ba679d71da57407a01
joeikedo <54558160+joeikedo@users.noreply.github.com>
2021-06-02 20:00:53 -0700

    back test

--- __tests__/script.test.js
+++ __tests__/script.test.js
@@ -69,13 +69,15 @@ describe('Basic user flow for SPA ', () => {
     expect(page.url()).toMatch('#archive');
   });
 
-  /*
+  
   //Testing the back button
   test('Back arrow button', async() => {
+    let archive_button = await page.$('#archive');
+    await archive_button.click();
     await page.goBack(); 
     expect(page.url()).toBe("https://nbuhr9.github.io/test-server/"); 
   });
-  */
+  
 
 
 
