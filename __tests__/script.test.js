318b460e452bc3777024c29d76ed3b02877f906d
joeikedo <54558160+joeikedo@users.noreply.github.com>
2021-06-02 20:19:36 -0700

    all

--- __tests__/script.test.js
+++ __tests__/script.test.js
@@ -63,10 +63,21 @@ describe('Basic user flow for SPA ', () => {
   });
 
    //Test for changing to archive page toggle
+  test('Home url test', async() => {
+    expect(page.url()).toMatch("https://nbuhr9.github.io/test-server/");
+  });
+
+  //Test for changing to archive page toggle
   test('Archive view url', async() => {
     let archive_button = await page.$('#archive');
     await archive_button.click();
     expect(page.url()).toMatch('#archive');
+  });
+
+  //Testing the back button
+  test('Back arrow button', async() => {
+      await page.goBack(); 
+      expect(page.url()).toBe("https://nbuhr9.github.io/test-server/"); 
   });
 
 
